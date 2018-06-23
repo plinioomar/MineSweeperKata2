f1eac9516fa097251e0da019813439dd0f90e095
plinioomar <plinioomar@yahoo.es>
2018-06-23 14:58:54 -0400

    New Methods Added to Lower Complexity

--- MineSweeperKataLibrary/FieldCollector.cs
+++ MineSweeperKataLibrary/FieldCollector.cs
@@ -18,27 +18,34 @@ namespace MineSweeperKataLibrary
         }
         public void GetFields(string path)
         {
-            bool endOfStream = false;
-            String curLine;
             int i = 0;
+            bool endOfStream = false;
             using (StreamReader reader = new StreamReader(path))
             {
-                while (!endOfStream)
-                {
-                    curLine = reader.ReadLine();
-                    if (curLine.Equals("0 0"))
-                        endOfStream = true;
-                    else if (i == 0)
-                    {
-                        infoBlock = curLine;
-                        i++;
-                    }
-                    else
-                        infoBlock = infoBlock + System.Environment.NewLine + curLine;
-                }
+                GetFieldloop(reader, endOfStream, i);
             }
 
             GetFields();
+        }
+
+        public void GetFieldloop(StreamReader reader, bool endOfStream, int i)
+        {
+
+            String curLine;
+            while (!endOfStream)
+            {
+
+                curLine = reader.ReadLine();
+                if (curLine.Equals("0 0"))
+                    endOfStream = true;
+                else if (i == 0)
+                {
+                    infoBlock = curLine;
+                    i++;
+                }
+                else
+                    infoBlock = infoBlock + System.Environment.NewLine + curLine;
+            }
         }
 
         private void GetFields()
@@ -48,42 +55,53 @@ namespace MineSweeperKataLibrary
 
             for (int i = 0; i < values.Length; i += 2)
             {
-                if (values[i].ToCharArray()[0] != '.' && values[i].ToCharArray()[0] != '*')
+                Verifier(values, i);
+            }
+
+
+        }
+        private void Verifier(String[] values, int i)
+        {
+            if (values[i].ToCharArray()[0] != '.' && values[i].ToCharArray()[0] != '*')
+            {
+                int N = Convert.ToInt32(values[i].ToCharArray()[0]) - 48;
+                int M = Convert.ToInt32(values[i].ToCharArray()[2]) - 48;
+
+                Field field = new Field(N, M);
+
+                for (int j = i + 2, k = 0; j <= i + (N * 2) && k < field.Lines; j += 2, k++)
                 {
-                    int N = Convert.ToInt32(values[i].ToCharArray()[0]) - 48;
-                    int M = Convert.ToInt32(values[i].ToCharArray()[2]) - 48;
-
-                    Field field = new Field(N, M);
-
-                    for (int j = i + 2, k = 0; j <= i + (N * 2) && k < field.Lines; j += 2, k++)
-                    {
-                        field.InputGrid[k] = values[j];
-                    }
-                    fields.Add(field);
-                    i += (N * 2);
+                    field.InputGrid[k] = values[j];
                 }
+                fields.Add(field);
+                i += (N * 2);
             }
         }
 
         public void GenerateOutput(string path)
         {
             GetFields(path);
-            foreach(Field field in fields)
+            foreach (Field field in fields)
             {
                 field.GenerateOutputField();
             }
 
-            using(StreamWriter writer = new StreamWriter("C:\\Users\\plini\\Desktop\\Test_Cases_Text_Files\\Output.txt"))
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\plini\\Desktop\\Test_Cases_Text_Files\\Output.txt"))
             {
-                for(int i = 0; i < fields.Count; i++)
+                for (int i = 0; i < fields.Count; i++)
                 {
-                    writer.WriteLine("Field #" + (i + 1) + ":");
-                    foreach (String line in fields[i].OutputGrid)
-                        writer.WriteLine(line);
-                    if (!(i + 1 >= fields.Count))
-                        writer.WriteLine();
+                    FieldWriter(writer, i);
                 }
             }
+        }
+
+        public void FieldWriter(StreamWriter writer, int i)
+        {
+            writer.WriteLine("Field #" + (i + 1) + ":");
+            foreach (String line in fields[i].OutputGrid)
+                writer.WriteLine(line);
+            if (!(i + 1 >= fields.Count))
+                writer.WriteLine();
         }
     }
 }
